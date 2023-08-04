94f2bc09ab2f5ff905b53205dcb4812f95a341cb
Vikas Sahu <realhacker2015@gmail.com>
2023-08-04 15:01:47 +0530

    ok

---
+++ components/navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import { Sparkles } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { MobileSidebar } from "@/components/mobile-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { useProModal } from "@/hooks/use-pro-modal";
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
+interface NavbarProps {
+  isPro: boolean;
+}
+
+export const Navbar = ({ isPro }: NavbarProps) => {
+  const proModal = useProModal();
+
+  return (
+    <div className="fixed z-50 flex items-center justify-between w-full h-16 px-4 py-2 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <MobileSidebar isPro={isPro} />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            Gita GPT
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        {!isPro && (
+          <Button onClick={proModal.onOpen} size="sm" variant="premium">
+            Upgrade
+            <Sparkles className="w-4 h-4 ml-2 text-white fill-white" />
+          </Button>
+        )}
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
